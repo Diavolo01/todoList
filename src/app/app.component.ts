ed5a39c63a6dfc7df69df260d8d6ba4f05513fc9
Diavolo01 <https://github.com/Diavolo01>
2024-12-25 14:38:50 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'todoList';
+  taskArray = [{taskName: 'do smth', isCompleted: false}]
+
+  onSubmit(form: NgForm){
+    console.log(form);
+
+    this.taskArray.push({
+       taskName: form.controls['task'].value,
+       isCompleted: false
+    })
+  }
+  onDelete(index: number){
+    this.taskArray.splice(index, 1);
+    console.log(index);
+  }
+  onCheck(index: number) {
+    console.log(this.taskArray)
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
 }
+
